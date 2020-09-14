f064332b2002fa5afb7b9ace986ddfcfb87381ac
Michael Solomon <6020319+mtsolmn@users.noreply.github.com>
2020-09-14 10:34:06 -0500

    CHANGELOG: commit to merge lantzproject/lantz and lantzproject/lantz-core into single repository 1. migrate docs from lantz to lantz-core 2. migrate lantz/__init__.py and lantz/__main__.py to lantz-core 3. update MANIFEST.in 4. migrate README from lantz to lantz-core 5. migrate and update requirements-doc.txt requirements-full.txt requirements-github.txt requirements-rtd.txt 6. complete rewrite of setup.py: merge settings from lantz to lantz-core, update dependencies, update general settings 7. migrate scripts/lantz-monitor.py from lantz to lantz-core

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from setuptools import setup
-
-import os
 import codecs
 
 
@@ -15,54 +10,86 @@ long_description = '\n\n'.join([read('README'),
                                 read('AUTHORS'),
                                 read('CHANGES')])
 
-__doc__ = long_description
-
-root_folder = os.path.dirname(os.path.abspath(__file__))
-
-
-setup(name='lantz-core',
-      version='0.5.4.dev0',
-      license='BSD',
-      description='Instrumentation framework',
-      long_description=long_description,
-      keywords='measurement control instrumentation science',
-      author='Hernan E. Grecco',
-      author_email='hernan.grecco@gmail.com',
-      url='https://github.com/lantzproject',
-      packages=['lantz.core'],
-      test_suite='lantz.core.testsuite.testsuite',
-      python_requires='>=3.6',
-      install_requires=['pint>=0.6',
-                        'pyvisa>=1.6.2',
-                        'stringparser',
-                        'pimpmyclass>=0.4.2',
-                        'PySignal>=1.1.1',
-                        'pyyaml',
-                        'serialize'],
-      extras_require={
-          'color': ['colorama']
-      },
-      zip_safe=False,
-      platforms='any',
-      entry_points={
-          'console_scripts': [
-              'lantz-config = lantz.core.__main__:config'
-          ],
-          'lantz_subcommands': [
-              'config = lantz.core.__main__:config'
-          ],
-      },
-      classifiers=[
-           'Development Status :: 4 - Beta',
-           'Intended Audience :: Developers',
-           'Intended Audience :: Science/Research',
-           'License :: OSI Approved :: BSD License',
-           'Operating System :: MacOS :: MacOS X',
-           'Operating System :: Microsoft :: Windows',
-           'Operating System :: POSIX',
-           'Programming Language :: Python',
-           'Programming Language :: Python :: 3.6',
-           'Topic :: Scientific/Engineering',
-           'Topic :: Software Development :: Libraries'
-      ],
+setup(
+    name='lantz',
+    version='0.6.0',
+    license='BSD 3-Clause License',
+    description='Simple yet powerful instrumentation in Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lantzproject/lantz',
+    author='Hernan E. Grecco',
+    author_email='hernan.grecco@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Widget Sets',
+        'Topic :: System :: Hardware',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+        'Topic :: System :: Logging',
+        'Topic :: System :: Monitoring',
+        'Topic :: Utilities',
+    ],
+    keywords='lantz, hardware interface, instrumentation framework, science, research',
+    packages=['lantz', 'lantz.core'],
+    zip_safe=False,
+    python_requires='>=3.6, <4',
+    install_requires=[
+        'pimpmyclass>=0.4.3',
+        'pint>=0.16',
+        'pyvisa>=1.10.1',
+        'pysignal>=1.1.1',
+        'pyyaml>=5.3.1',
+        'serialize>=0.1',
+        'stringparser>=0.5',
+        'lantz-drivers>=0.5.2',
+        'lantz-sims>=0.5.2',
+    ],
+    extras_require={
+        'qt': [
+            'lantz-qt>=0.5.3',
+        ],
+        'ino': [
+            'lantz-ino>=0.5.2',
+        ],
+        'full': [
+            'lantz-qt>=0.5.3',
+            'lantz-ino>=0.5.2',
+        ],
+        'color': [
+            'colorama>=0.4.3',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'lantz = lantz.__main__:main',
+            'lantz-config = lantz.core.__main__:config',
+        ],
+        'lantz_subcommands': [
+            'config = lantz.core.__main__:config',
+        ],
+    },
+    test_suite='lantz.core.testsuite.testsuite',
+    project_urls={
+        'Bug Reports': 'https://github.com/lantzproject/lantz/issues',
+        'Source': 'https://github.com/lantzproject/lantz/',
+    },
+    include_package_data=True,
+    options={'bdist_wheel': {'universal': '1'}},
 )
